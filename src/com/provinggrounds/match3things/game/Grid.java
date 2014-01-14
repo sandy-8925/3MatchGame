f1ec87b32e31dcb413604602747e2c7ba74e46fe
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-14 15:57:10 -0800

    Create new class Grid to represent the game grid

---
+++ src/com/provinggrounds/match3things/game/Grid.java
@@ -0,0 +1,15 @@
+package com.provinggrounds.match3things.game;
+
+
+/*
+ * Represents a game grid. Contains a rectangular grid of Block objects.
+ */
+public class Grid {
+    int width;
+    int height;
+
+    Grid(int width, int height) {
+	this.width = width;
+	this.height = height;
+    }
+}
