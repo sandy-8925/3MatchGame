057a0d6a61d830c8aed096057114c9934c7b4a8e
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-14 16:05:40 -0800

    Grid: Add 2-dimensional Block array as member, and create private default constructor

--- src/com/provinggrounds/match3things/game/Grid.java
+++ src/com/provinggrounds/match3things/game/Grid.java
@@ -7,6 +7,15 @@ package com.provinggrounds.match3things.game;
 public class Grid {
     int width;
     int height;
+    Block[][] gameGrid;
+
+    /*
+     * Width and height must always be provided to Grid - should not be constructed without initializing/passing in those values
+     */
+    @SuppressWarnings("unused")
+    private Grid() {
+
+    }
 
     Grid(int width, int height) {
 	this.width = width;
