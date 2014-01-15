e00b8d69f358c3488fffd5a095b8b83a5b72b404
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-14 17:29:27 -0800

    Grid: Add code to fill game grid with random numbers

--- src/com/provinggrounds/match3things/game/Grid.java
+++ src/com/provinggrounds/match3things/game/Grid.java
@@ -1,5 +1,7 @@
 package com.provinggrounds.match3things.game;
 
+import java.util.Random;
+
 
 /*
  * Represents a game grid. Contains a rectangular grid of numbers.
@@ -13,16 +15,17 @@ public class Grid {
      */
     int numObjectTypes;
     int[][] gameGrid;
+    Random randomNumberGenerator;
 
     /*
      * Width and height must always be provided to Grid - should not be constructed without initializing/passing in those values
      */
-    @SuppressWarnings("unused")
     private Grid() {
-
+	randomNumberGenerator = new Random();
     }
 
     Grid(int width, int height, int numObjectTypes) {
+	this();
 	this.width = width;
 	this.height = height;
 	this.numObjectTypes = numObjectTypes;
@@ -41,6 +44,10 @@ public class Grid {
      * Initialize/fill game grid with random objects
      */
     private void fillGameGrid() {
-
+	for( int[] gameGridRow : gameGrid ) {
+	    for(int counter = 0; counter < gameGridRow.length; counter++) {
+		gameGridRow[counter] = randomNumberGenerator.nextInt(numObjectTypes) + 1;
+	    }
+	}
     }
 }
