bb24d53802958aaa65a1fe6f7c555e6e3cc3035d
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-29 00:42:54 -0800

    Create new activity - NewGamePreferencesActivity. User will set preferences for the new game.

---
+++ src/com/provinggrounds/match3things/activity/NewGamePreferencesActivity.java
@@ -0,0 +1,15 @@
+package com.provinggrounds.match3things.activity;
+
+import com.proving.grounds.match3things.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewGamePreferencesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_new_game_preferences);
+    }
+
+}
