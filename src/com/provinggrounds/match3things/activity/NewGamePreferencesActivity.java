438e298e8f4c9b9c30c8e30338fd94f02d083afe
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-02-01 14:22:45 -0800

    NewGamePreferencesActivity: On clicking OK button, start the GameActivity and send it the width, height and number of block types

--- src/com/provinggrounds/match3things/activity/NewGamePreferencesActivity.java
+++ src/com/provinggrounds/match3things/activity/NewGamePreferencesActivity.java
@@ -1,8 +1,13 @@
 package com.provinggrounds.match3things.activity;
 
 import com.proving.grounds.match3things.R;
+import com.provinggrounds.match3things.constants.IntentExtraReferences;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.app.Activity;
+import android.content.Intent;
 
 public class NewGamePreferencesActivity extends Activity {
 
@@ -12,4 +17,19 @@ public class NewGamePreferencesActivity extends Activity {
 	setContentView(R.layout.activity_new_game_preferences);
     }
 
+    /*
+     * Called on clicking OK button
+     */
+    public void launchNewGame(View clickedView) {
+	int height = Integer.parseInt(((EditText) findViewById(R.id.Input_Height)).getText().toString());
+	int width = Integer.parseInt(((EditText) findViewById(R.id.Input_Width)).getText().toString());
+	int numBlockTypes = Integer.parseInt(((EditText) findViewById(R.id.Input_NumBlockTypes)).getText().toString());
+
+	Intent startGameActivityIntent = new Intent(this, GameActivity.class);
+	startGameActivityIntent.putExtra(IntentExtraReferences.HEIGHT, height);
+	startGameActivityIntent.putExtra(IntentExtraReferences.WIDTH , width);
+	startGameActivityIntent.putExtra(IntentExtraReferences.NUMBLOCKTYPES , numBlockTypes);
+
+	startActivity(startGameActivityIntent);
+    }
 }
---
+++ src/com/provinggrounds/match3things/constants/IntentExtraReferences.java
@@ -0,0 +1,7 @@
+package com.provinggrounds.match3things.constants;
+
+public class IntentExtraReferences {
+    public static final String HEIGHT = "height";
+    public static final String WIDTH = "width";
+    public static final String NUMBLOCKTYPES = "numBlockTypes";
+}
