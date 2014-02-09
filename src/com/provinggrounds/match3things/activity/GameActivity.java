143d0b4161af66c42ac72b36f1926d4874449f64
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-02-08 20:29:47 -0800

    GameActivity: Remove onOptionsItemSelected and NavUtils import

--- src/com/provinggrounds/match3things/activity/GameActivity.java
+++ src/com/provinggrounds/match3things/activity/GameActivity.java
@@ -7,10 +7,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.support.v4.app.NavUtils;
 
 public class GameActivity extends Activity {
 
@@ -56,23 +54,6 @@ public class GameActivity extends Activity {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.game, menu);
 	return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-	switch (item.getItemId()) {
-	case android.R.id.home:
-	    // This ID represents the Home or Up button. In the case of this
-	    // activity, the Up button is shown. Use NavUtils to allow users
-	    // to navigate up one level in the application structure. For
-	    // more details, see the Navigation pattern on Android Design:
-	    //
-	    // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-	    //
-	    NavUtils.navigateUpFromSameTask(this);
-	    return true;
-	}
-	return super.onOptionsItemSelected(item);
     }
 
 }
