47b531abc81250ab10a0741856ed8cff2971918e
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-02-10 20:48:43 -0800

    Add new class Coord to store object coordinates within grid

---
+++ src/com/provinggrounds/match3things/game/Coord.java
@@ -0,0 +1,18 @@
+package com.provinggrounds.match3things.game;
+
+public class Coord {
+	int x;
+	int y;
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Coord(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
