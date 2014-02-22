8b84b61b977d60f755db1b5e12de0a5fe529acf9
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-02-21 23:21:14 -0800

    MatchingSet: Override and implement toString

--- src/com/provinggrounds/match3things/game/MatchingSet.java
+++ src/com/provinggrounds/match3things/game/MatchingSet.java
@@ -23,4 +23,22 @@ public class MatchingSet {
     public void setCoordinates(Coord[] coordinates) {
         this.coordinates = coordinates;
     }
+
+    /**
+     * Returns all information about the matching set in a particular
+     * format which is subject to change.
+     *
+     * @return    String representation of MatchingSet object
+     *
+     * If any of the Coord elements are null, they will be skipped
+     */
+    @Override
+    public String toString() {
+	StringBuilder matchSetString = new StringBuilder("");
+	matchSetString.append(String.format("Match %d(%d)):", length(), blockType));
+	for(Coord currentPoint : coordinates) {
+	   if(currentPoint!=null) matchSetString.append(String.format(" %s", currentPoint.toString()));
+	}
+	return matchSetString.toString();
+    }
 }
